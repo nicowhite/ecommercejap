660e3402e7a5261d42593dd647e9d3f741c34349
Nicolas Blanco <nblancoestefan@gmail.com>
2021-09-03 19:56:30 -0300

    last changes

--- js/login.js
+++ js/login.js
@@ -1,12 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", function (e) {
   document.getElementById("logUser").addEventListener("click", function (e) {
-    let yourUser = document.getElementById("logU").value;
+    let yourUser = document.getElementById("logU");
     let yourPassword = document.getElementById("passU");
     if (yourPassword.value != '' && yourUser.value != '') {
       window.location.href = "./home.html";
     }
-    localStorage.setItem("yourUser", yourUser);
+    localStorage.setItem("username", yourUser.value);
   });
 });
 
