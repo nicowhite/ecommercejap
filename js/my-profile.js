d8e0d84eaed46cf786226434c000da9104c00442
Nicolas Blanco <nblancoestefan@gmail.com>
2021-10-20 09:58:01 -0300

    entrega 6

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,13 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+$("#profButton").click(function () {
+  localStorage.setItem("profname", $("#profName").val());
+  localStorage.setItem("proflast", $("#profLast").val());
+  localStorage.setItem("profemail", $("#profEmail").val());
+  localStorage.setItem("profage", $("#profAge").val());
+  localStorage.setItem("profphone", $("#profPhone").val());
 });
+
+$("#profName").val(localStorage.getItem("profname"));
+$("#profLast").val(localStorage.getItem("proflast"));
+$("#profEmail").val(localStorage.getItem("profemail"));
+$("#profAge").val(localStorage.getItem("profage"));
+$("#profPhone").val(localStorage.getItem("profphone"));
